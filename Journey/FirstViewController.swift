813552bbf414b5a3f034e6eeb32bb9503ae9522f
Aaron Tong <aaron.yue.tong@gmail.com>
2019-05-27 10:27:15 -0400

    Initial Commit

---
+++ Journey/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Journey
+//
+//  Created by Aaron Tong on 5/27/19.
+//  Copyright © 2019 Aaron Tong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Journey/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Journey
+//
+//  Created by Aaron Tong on 5/27/19.
+//  Copyright © 2019 Aaron Tong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
